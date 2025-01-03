f797d3d67c5c325f396b832014700fb5a03cef5e
Adrian Siekierka <kontakt@asie.pl>
2025-01-03 09:52:58 +0100

    tweak scale down expression

--- src/Color.h
+++ src/Color.h
@@ -75,7 +75,7 @@ constexpr channel_t scale_up(channel_t value, unsigned shift) {
 
 // scale down value, rounding evenly
 constexpr channel_t scale_down(channel_t value, unsigned shift) {
-  return (value + (1 << (shift - 1))) >> shift;
+  return (value * ((1 << shift) - 1)) / 255;
 }
 
 //
